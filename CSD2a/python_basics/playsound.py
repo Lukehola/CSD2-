83ae630fc77519e64cf7b66720cd5b91aa73d63f
Lukehola <lukeschraven@gmail.com>
2022-09-13 10:19:57 +0200

    Even laten zien dat ik ook functies kan schrijven

--- CSD2a/python_basics/playsound.py
+++ CSD2a/python_basics/playsound.py
@@ -8,13 +8,18 @@ t = np.linspace(0, seconds, seconds*fs, False)
 
 number_of_times_played = int(input("How many times do you want it to be played?"))
 
-##def number_of_times_function(): 
+##def number_of_times_function(number_of_times_played): 
+    ##for in in range (number_of_times_played)
+    ##play_obj = sa.play_buffer(audio, 1, 2, fs)
+    ##play_obj.wait_done()
 
 note = np.sin(frequency*t*2*np.pi)
 
 audio = note * (2**15 - 1) / np.max(np.abs(note)) 
 
 audio = audio.astype(np.int16)
+
+##number_of_times_function
 
 for i in (range(number_of_times_played)): 
     play_obj = sa.play_buffer(audio, 1, 2, fs)
