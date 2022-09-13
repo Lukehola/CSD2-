dd1f7825070f18cb6f0adfe64e1399c5ab4720eb
Lukehola <lukeschraven@gmail.com>
2022-09-13 09:55:30 +0200

    goede loop

--- CSD2a/python_basics/playsound.py
+++ CSD2a/python_basics/playsound.py
@@ -8,6 +8,7 @@ t = np.linspace(0, seconds, seconds*fs, False)
 
 number_of_times_played = int(input("How many times do you want it to be played?"))
 
+##def number_of_times_function(): 
 
 note = np.sin(frequency*t*2*np.pi)
 
@@ -15,7 +16,7 @@ audio = note * (2**15 - 1) / np.max(np.abs(note))
 
 audio = audio.astype(np.int16)
 
-play_obj = sa.play_buffer(audio, 1, 2, fs)
-
-for i in range(number_of_times_played): 
+for i in (1 + range(number_of_times_played)): 
+    play_obj = sa.play_buffer(audio, 1, 2, fs)
     play_obj.wait_done()
+    print(i)
