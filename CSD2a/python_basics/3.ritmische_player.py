5e3ff6161ff0982df697a910ed281fdeb12725fb
Lukehola <lukeschraven@gmail.com>
2022-09-14 02:04:36 +0200

    2de wav file toegevoegd

--- CSD2a/python_basics/3.ritmische_player.py
+++ CSD2a/python_basics/3.ritmische_player.py
@@ -3,15 +3,14 @@ import simpleaudio as sa
 import math 
 import sys 
 import array as array_module 
+from random import random
 
 wave_obj = sa.WaveObject.from_wave_file("/Users/521459/Desktop/CSD2-/CSD2a/python_basics/SFX_schaak.wav")
-##wave_obj_2 = sa.WaveObject.from_wave_file("/Users/521459/Desktop/CSD2-/CSD2a/python_basics/enpassant.wav")
-##play_obj = wave_obj.play()
-##play_obj.wait_done()
+wave_obj_2 = sa.WaveObject.from_wave_file("/Users/521459/Desktop/CSD2-/CSD2a/python_basics/enpassant.wav")
+
+##def variable for true bpm adjusted time 
 
 true_time_tobeadded = 0 
-
-
 
 input_list = [4, 1, 0.5, 1.5, 0.5, 120]
 bpm = input_list[-1]
@@ -24,11 +23,6 @@ print(second_apart_list)
 
 true_time_list = []
 
-##remove line amount and bpm from the list 
-
-
-
-
 ##BPM calculation, decides how fast it should be played 
 
 for i in range(len(second_apart_list)): 
@@ -40,7 +34,11 @@ for i in range(len(second_apart_list)):
 
 def number_of_measures(): 
         for seconds in true_time_list: 
-            play_obj = wave_obj.play()
+            random_number = (random()) 
+            if random_number >= 0.5: 
+                wave_obj.play()
+            else :
+                wave_obj_2.play()
             time.sleep(seconds)
 
 for i in range(input_list[0]): 
