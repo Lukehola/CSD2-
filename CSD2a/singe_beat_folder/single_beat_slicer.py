f5a65287b60e8addf8b3f196402e0b81b6b0f979
Lukehola <lukeschraven@gmail.com>
2022-09-21 14:32:51 +0200

    Fix playback

--- CSD2a/singe_beat_folder/single_beat_slicer.py
+++ CSD2a/singe_beat_folder/single_beat_slicer.py
@@ -8,10 +8,12 @@ noteDuration = [0.25, 0.5, 1, 2.25]
 timeStamps16th = []
 bpmNoteTrue = []
 
+startTime = time.time()
+
 wave_obj = sa.WaveObject.from_wave_file("/Users/521459/Desktop/CSD2-/CSD2a/ritmische_player_folder/3a.SFX_schaak.wav")
 wave_obj_2 = sa.WaveObject.from_wave_file("/Users/521459/Desktop/CSD2-/CSD2a/ritmische_player_folder/3a.enpassant.wav")
 
-bpm = 100
+bpm = 30
 bpm_adjust = 60/bpm
 
 def note_to_16():
@@ -37,13 +39,28 @@ def change_it_up():
         bpmNoteTrue.pop(2)
         bpmNoteTrue.append(random_number)
 
-
-
-
-
 bpm_adjust_16()
 note_to_16()
 change_it_up()
 
-print(bpmNoteTrue)
-print(timeStamps16th)
+# print(bpmNoteTrue)
+# print(timeStamps16th)
+# timestamp = bpmNoteTrue.pop(0)
+# print(len(bpmNoteTrue))
+
+timestamp = bpmNoteTrue.pop(0)
+
+while PlayBackLoop: 
+    timeNow = time.time()
+    if(timeNow - startTime >= timestamp): 
+        wave_obj.play()
+        if (len(bpmNoteTrue) > 0 ): 
+            timestamp = bpmNoteTrue.pop(0)
+    
+        else:
+            PlayBackLoop = False  
+
+    else: 
+        time.sleep(0.001)
+
+time.sleep(1)
