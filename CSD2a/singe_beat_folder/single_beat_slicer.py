b171a663b5c926bbbf75f5e53194aa72d123b5c5
Lukehola <lukeschraven@gmail.com>
2022-09-21 14:09:58 +0200

    eerste functies

--- CSD2a/singe_beat_folder/single_beat_slicer.py
+++ CSD2a/singe_beat_folder/single_beat_slicer.py
@@ -1,6 +1,8 @@
 import simpleaudio as sa 
 import time 
+import random 
 
+PlayBackLoop = True 
 
 noteDuration = [0.25, 0.5, 1, 2.25]
 timeStamps16th = []
@@ -22,9 +24,26 @@ def bpm_adjust_16():
     for i in noteDuration: 
         bpmNoteTrue.append(i*bpm_adjust)
         
+def change_it_up():     
+    random_number = random.uniform(0,3)
+
+    if 0 <= random_number <= 1: 
+        bpmNoteTrue.reverse()
+
+    elif 1 <= random_number <= 2:
+        random.shuffle(bpmNoteTrue)
+
+    else: 
+        bpmNoteTrue.pop(2)
+        bpmNoteTrue.append(random_number)
+
+
+
+
 
 bpm_adjust_16()
 note_to_16()
+change_it_up()
 
 print(bpmNoteTrue)
 print(timeStamps16th)
